24e15f423b0028f0bed874962a54c028a4795be6
Ayyappa1234 <ayyappadhul@gmail.com>
2022-10-28 20:57:22 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // URI: "mongodb://127.0.0.1/car_store",
+  URI: "mongodb+srv://Ayyappa2506:Hanvith%401069@myfirstcluster.wlkuvvg.mongodb.net/car_store?retryWrites=true&w=majority",
+  
+};
